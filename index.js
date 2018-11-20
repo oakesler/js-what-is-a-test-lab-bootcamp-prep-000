5d5efca4229807ffca0596c9cdb6f6ec66faa246
Olivia Kesler <oakesler@protonmail.ch>
2018-11-20 19:05:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// write your code below!
+
 var name = "Susan"
 var height = 39
 var message = ` is ${height} inches tall`
 
-module.exports = {height, message}
+module.exports = {name, height, message}
